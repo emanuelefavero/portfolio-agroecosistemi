a2e78b41a58f5f943e796c34d0cc3b9cdd2e7275
emanuelefavero <59388889+emanuelefavero@users.noreply.github.com>
2021-01-23 08:22:21 +0100

    First file upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// DECLARE VARIABLES
+// Dropdown Menu
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+// Menu Icon (Hamburger)
+const menuIcon = document.querySelector('.hamburger-menu-icon');
+const menuIconLineTop = document.querySelector(
+    '.hamburger-menu-icon .line-top');
+const menuIconLineCenter = document.querySelector(
+    '.hamburger-menu-icon .line-center');
+const menuIconLineBottom = document.querySelector(
+    '.hamburger-menu-icon .line-bottom');
+
+// Dropdown Menu Anchors
+const dropdownMenuAnchors = document.querySelectorAll(
+    '.dropdown-menu a');
+
+// DECLAR DRY FUNCTION (Don't Repeat Yourself)
+function toggleDropdownClasses() {
+    dropdownMenu.classList.toggle('active');
+    dropdownMenu.classList.toggle('opacity');
+
+    menuIconLineTop.classList.toggle('rotate');
+    menuIconLineCenter.classList.toggle('move');
+    menuIconLineBottom.classList.toggle('rotate');
+}
+
+// OPEN DROPDOWN MENU WHEN HAMBURGER ICON IS CLICKED
+// ANIMATE HAMBURGER ICON TO X
+menuIcon.addEventListener('click', toggleDropdownClasses);
+
+// CLOSE DROP DOWN MENU EACH TIME LINK IS CLICKED
+for (let i = 0; i < dropdownMenuAnchors.length; i++) {
+    dropdownMenuAnchors[i].addEventListener('click',
+        toggleDropdownClasses);
+}
